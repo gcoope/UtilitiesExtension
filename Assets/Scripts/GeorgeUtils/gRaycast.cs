2d96c483c18d2cf281aeb1db9177fda6f4fbd905
George Coope <george.a.coope@gmail.com>
2014-07-17 13:06:00 +0100

    Added gRaycast

---
+++ Assets/Scripts/GeorgeUtils/gRaycast.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GeorgeUtils
+{
+	public class gRaycast {
+		/// <summary>
+		/// Returns name of gameobject clicked on.
+		/// </summary>
+		/// <returns>The gameobject name.</returns>
+		public static string RaycastColliderName(){
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit, 100f)) {
+				return hit.collider.gameObject.name;
+			} else {
+				return "null";
+			}
+		}
+
+		/// <summary>
+		/// Raycasts down.
+		/// </summary>
+		/// <returns><c>true</c>, if raycast hits, <c>false</c> otherwise.</returns>
+		/// <param name="distance">Distance.</param>
+		/// <param name="gameobject">Gameobject.</param>
+		public static bool RaycastDown(float distance, GameObject gameobject) {
+			Vector3 down = new Vector3 (0, -1, 0);
+			if (Physics.Raycast (gameobject.transform.position, down, distance)) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+
+	}
+}
